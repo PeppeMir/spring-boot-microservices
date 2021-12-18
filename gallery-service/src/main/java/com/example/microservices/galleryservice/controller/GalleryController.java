72c85b40adca8d8d202fdde20b5291b10cdd7999
PeppeMir <giuseppe.mrgl@gmail.com>
2021-12-18 17:58:19 +0000

    Fixed logging to show load balancing effectivness

--- gallery-service/src/main/java/com/example/microservices/galleryservice/controller/GalleryController.java
+++ gallery-service/src/main/java/com/example/microservices/galleryservice/controller/GalleryController.java
@@ -1,15 +1,11 @@
 package com.example.microservices.galleryservice.controller;
 
-import java.text.MessageFormat;
 import java.util.List;
 
 import com.example.microservices.galleryservice.model.Gallery;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,18 +17,11 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/galleries")
 public class GalleryController {
-    private final Logger log = LoggerFactory.getLogger(GalleryController.class);
-
     @Autowired
     private WebClient.Builder webClientBuilder;
 
-    @Autowired
-    private Environment env;
-
     @GetMapping("/{id}")
     public Mono<Gallery> get(@PathVariable Integer id) {
-        log.info(MessageFormat.format("Gallery Service running at port {0} answering...", env.getProperty("local.server.port")));
-
 		return webClientBuilder.build().get().uri("http://image-service/images")
         				.retrieve()
                         .bodyToMono(new ParameterizedTypeReference<List<Object>>() {})
--- image-service/src/main/java/com/example/microservices/imageservice/controller/ImageController.java
+++ image-service/src/main/java/com/example/microservices/imageservice/controller/ImageController.java
@@ -1,10 +1,15 @@
 package com.example.microservices.imageservice.controller;
 
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.List;
 
 import com.example.microservices.imageservice.model.Image;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +19,15 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/images")
 public class ImageController {
+    private final Logger log = LoggerFactory.getLogger(ImageController.class);
 
+    @Autowired
+    private Environment env;
+    
     @GetMapping
     public Mono<List<Image>> getAll() {
+        log.info(MessageFormat.format("Gallery Service running at port {0} answering...", env.getProperty("local.server.port")));
+
         return Mono.just(Arrays.asList(
 			new Image(1, "Treehouse of Horror V", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3842005760"),
 			new Image(2, "The Town", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3698134272"),
