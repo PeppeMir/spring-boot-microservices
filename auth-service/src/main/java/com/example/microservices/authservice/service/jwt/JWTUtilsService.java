b1970d626fda14a272058a22ea51f3a7142be2be
PeppeMir <giuseppe.mrgl@gmail.com>
2025-08-17 16:52:08 +0100

    Migrated to configuration files in yaml format. Updated JWT library to use latest version. Moved to JWT encoding using HMAC with SHA-512 algorithm

--- auth-service/src/main/java/com/example/microservices/authservice/service/jwt/JWTUtilsService.java
+++ auth-service/src/main/java/com/example/microservices/authservice/service/jwt/JWTUtilsService.java
@@ -1,35 +1,43 @@
 package com.example.microservices.authservice.service.jwt;
 
-import java.util.Date;
-
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Service
 public class JWTUtilsService {
 
-	@Value("${jwt.secret}")
-	private String jwtSecret;
+    private final long tokenValidity;
+    private final SecretKey key;
 
-	@Value("${jwt.token.validity}")
-	private long tokenValidity;
+    @Autowired
+    public JWTUtilsService(
+            @Value("${jwt.secret}") final String jwtSecret,
+            @Value("${jwt.token.validity}") final long tokenValidity) {
+        this.tokenValidity = tokenValidity;
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
 
-	public String generateToken(final String userId, final String userName) {
-		final long nowMillis = System.currentTimeMillis();
+    public String generateToken(final String userId, final String userName) {
+        final long nowMillis = System.currentTimeMillis();
 
-		final Claims claims = Jwts	.claims()
-									.setSubject(userId);
-		claims.put("userName", userName);
+        final Claims claims = Jwts.claims()
+                .subject(userId)
+                .add("userName", userName)
+                .build();
 
-		return Jwts	.builder()
-					.setClaims(claims)
-					.setIssuedAt(new Date(nowMillis))
-					.setExpiration(new Date(nowMillis + tokenValidity))
-					.signWith(SignatureAlgorithm.HS512, jwtSecret)
-					.compact();
-	}
+        return Jwts.builder()
+                .claims(claims)
+                .issuedAt(new Date(nowMillis))
+                .expiration(new Date(nowMillis + tokenValidity))
+                .signWith(key, Jwts.SIG.HS512)
+                .compact();
+    }
 }
--- gateway-service/src/main/java/com/example/microservices/gatewayservice/service/jwt/JWTUtilsService.java
+++ gateway-service/src/main/java/com/example/microservices/gatewayservice/service/jwt/JWTUtilsService.java
@@ -1,47 +1,56 @@
 package com.example.microservices.gatewayservice.service.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import javax.crypto.SecretKey;
 
 @Service
 public class JWTUtilsService {
 
-	@Value("${jwt.secret}")
-	private String jwtSecret;
+    private final JwtParser parser;
 
-	public ClaimsData getClaims(final String token) {
-		try {
-			final Claims claims = Jwts	.parser()
-						.setSigningKey(jwtSecret)
-						.parseClaimsJws(token)
-						.getBody();
-			
-			final ClaimsData claimsData = new ClaimsData();
-			
-			claimsData.setUserId(claims.getSubject());
-			claimsData.setUserName(claims.get("userName", String.class));
-			
-			return claimsData;
-		} catch (final Exception e) {
-			System.out.println(e.getMessage() + " => " + e);
-			return null;
-		}
-	}
+    @Autowired
+    public JWTUtilsService(@Value("${jwt.secret}") final String jwtSecret) {
+        final SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+        this.parser = Jwts.parser()
+                .verifyWith(key)
+                .build();
+    }
 
-	public boolean isTokenValid(final String token) {
-		if (!StringUtils.hasText(token)) {
-			return false;
-		}
-		
-		// This will throw an exception in case the token is invalid
-		Jwts.parser()
-			.setSigningKey(jwtSecret)
-			.parseClaimsJws(token);
-		
-		return true;
-	}
+    public ClaimsData getClaims(final String token) {
+        try {
+            final Claims claims = parser
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            final ClaimsData claimsData = new ClaimsData();
+
+            claimsData.setUserId(claims.getSubject());
+            claimsData.setUserName(claims.get("userName", String.class));
+
+            return claimsData;
+        } catch (final Exception e) {
+            System.out.println(e.getMessage() + " => " + e);
+            return null;
+        }
+    }
+
+    public boolean isTokenValid(final String token) {
+        if (!StringUtils.hasText(token)) {
+            return false;
+        }
+
+        // This will throw an exception in case the token is invalid
+        parser.parseSignedClaims(token);
+
+        return true;
+    }
 }
